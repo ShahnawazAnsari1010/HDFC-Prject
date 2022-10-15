3238b481e98ff1b719e06793525d8eb03485ce6c
ShahnawazAnsari1010 <115578519+ShahnawazAnsari1010@users.noreply.github.com>
2022-10-15 17:19:58 +0530

    Add files via upload

---
+++ CourierEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDMS.Entities
+{
+    public class CourierEntity : BaseEntity
+    {
+        public int? ID { get; set; }
+        public string Courier { get; set; }
+
+    }
+}
