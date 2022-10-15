8a2b30e119084fca4364f665d63f18486858796b
ShahnawazAnsari1010 <115578519+ShahnawazAnsari1010@users.noreply.github.com>
2022-10-15 17:20:55 +0530

    Add files via upload

---
+++ ICourierRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GDMS.Repository.Interfaces
+{
+    public interface ICourierRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        string Add(T entity);
+        T Get(int id);
+        string Update(T entity);
+        bool Delete(int id);
+    }
+}
